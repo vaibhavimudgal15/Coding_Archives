06eda6e9e517f173e083dde10849f46caccd7130
Vaibhavi Mudgal <126332567+vaibhavimudgal15@users.noreply.github.com>
2025-07-06 10:37:59 +0530

    Time: 310 ms (23.17%) | Memory: 189.5 MB (29.85%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
@@ -1,49 +1,28 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class FindSumPairs {
-    vector<int> nums1, nums2;
-    unordered_map<int, int> hm; // Frequency map for nums2
+private:
+    vector<int> n1, n2;
+    unordered_map<int, int> m;
 
 public:
     FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
-        this->nums1 = nums1;
-        this->nums2 = nums2;
-
-        // Build frequency map for nums2
-        for (int num : nums2) {
-            hm[num]++;
+        n1 = nums1;
+        n2 = nums2;
+        for (int x : n2) {
+            m[x]++;
         }
     }
 
-    void add(int index, int val) {
-        // Update frequency map before changing nums2[index]
-        int oldVal = nums2[index];
-        hm[oldVal]--;
-
-        if (hm[oldVal] == 0) {
-            hm.erase(oldVal);
-        }
-
-        // Update nums2 value
-        nums2[index] += val;
-        int newVal = nums2[index];
-
-        // Add new value to the frequency map
-        hm[newVal]++;
+    void add(int i, int v) {
+        m[n2[i]]--;
+        n2[i] += v;
+        m[n2[i]]++;
     }
 
-    int count(int tot) {
-        int count = 0;
-
-        // For each element in nums1, check for complements in nums2
-        for (int num : nums1) {
-            int complement = tot - num;
-            if (hm.count(complement)) {
-                count += hm[complement];
-            }
+    int count(int t) {
+        int c = 0;
+        for (int x : n1) {
+            c += m[t - x];
         }
-        return count;
+        return c;
     }
 };
