792f6b73880582668edf0b0e6e9d007e86da8449
Vaibhavi Mudgal <126332567+vaibhavimudgal15@users.noreply.github.com>
2025-07-06 10:37:23 +0530

    Time: 89 ms (63.05%) | Memory: 81.9 MB (87.68%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
@@ -1,45 +1,49 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 class FindSumPairs {
+    vector<int> nums1, nums2;
+    unordered_map<int, int> hm; // Frequency map for nums2
+
 public:
-    vector<int> nums1;
-    vector<int> nums2;
-    unordered_map<int, int> mp1;
-    unordered_map<int, int> mp2;
     FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
         this->nums1 = nums1;
         this->nums2 = nums2;
-        for (auto it : nums1)
-        {
-            mp1[it]++;
-        }
-        for (auto it : nums2)
-        {
-            mp2[it]++;
+
+        // Build frequency map for nums2
+        for (int num : nums2) {
+            hm[num]++;
         }
     }
-    
+
     void add(int index, int val) {
-        mp2[nums2[index]]--;
+        // Update frequency map before changing nums2[index]
+        int oldVal = nums2[index];
+        hm[oldVal]--;
+
+        if (hm[oldVal] == 0) {
+            hm.erase(oldVal);
+        }
+
+        // Update nums2 value
         nums2[index] += val;
-        mp2[nums2[index]]++;
+        int newVal = nums2[index];
+
+        // Add new value to the frequency map
+        hm[newVal]++;
     }
-    
+
     int count(int tot) {
-        int cnt = 0;
-        for (auto& it : mp1)
-        {
-            int temp = tot - it.first;
-            if (mp2[temp] != 0)
-            {
-                cnt += it.second * mp2[temp];
+        int count = 0;
+
+        // For each element in nums1, check for complements in nums2
+        for (int num : nums1) {
+            int complement = tot - num;
+            if (hm.count(complement)) {
+                count += hm[complement];
             }
         }
-        return cnt;
+        return count;
     }
 };
-
-/**
- * Your FindSumPairs object will be instantiated and called as such:
- * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
- * obj->add(index,val);
- * int param_2 = obj->count(tot);
- */
