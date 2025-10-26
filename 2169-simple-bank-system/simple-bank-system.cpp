2138251831766c0f2eac1ef6ccfb1a6f3080f6f8
Vaibhavi Mudgal <126332567+vaibhavimudgal15@users.noreply.github.com>
2025-10-26 12:22:51 +0530

    Time: 0 ms (100.00%) | Memory: 123.6 MB (60.18%) - LeetSync

---
+++ 2169-simple-bank-system/simple-bank-system.cpp
@@ -0,0 +1,36 @@
+class Bank {
+public:
+    vector<long long> accounts;
+
+    Bank(vector<long long>& balance) { 
+        accounts = balance; 
+    }
+
+    bool transfer(int account1, int account2, long long money) {
+        if (account1 > accounts.size())
+            return false;
+        if (account2 > accounts.size())
+            return false;
+        if (accounts[account1 - 1] < money)
+            return false;
+        accounts[account1 - 1] -= money;
+        accounts[account2 - 1] += money;
+        return true;
+    }
+
+    bool deposit(int account, long long money) {
+        if (account > accounts.size())
+            return false;
+        accounts[account - 1] += money;
+        return true;
+    }
+
+    bool withdraw(int account, long long money) {
+        if (account > accounts.size())
+            return false;
+        if (accounts[account - 1] < money)
+            return false;
+        accounts[account - 1] -= money;
+        return true;
+    }
+};
